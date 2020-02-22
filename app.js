3a02c54b9e4c86e3b7fe8de5a29543c8150a9a8b
Wouter Nieuwerth <wouternieuwerth@gmail.com>
2020-02-22 19:43:57 +0100

    Bug fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
 const address = require('address')
 const body = require('body/form')
 const path = require('path')
