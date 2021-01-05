07f8e17411b77c8d81fc7db74e6971fb52a34b21
Wouter Nieuwerth <wouternieuwerth@gmail.com>
2021-01-05 15:00:15 +0100

    New mini dash

--- api/nest_thermostat.js
+++ api/nest_thermostat.js
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 const https = require('https')
+const http = require('http')
 const logger = require('../logger.js')
 const express = require('express')
 const router = express.Router()
@@ -7,14 +8,21 @@ const body = require('body/form')
 const secrets = require('../../private/private.js')
 
 const login_url = 'https://nestservices.google.com/partnerconnections/' + secrets.nest_project_id + '/auth?redirect_uri=' + secrets.nest_redirect_uri + '&access_type=offline&prompt=consent&client_id=' + secrets.nest_client_id + '&response_type=code&scope=https://www.googleapis.com/auth/sdm.service'
-var code, access_token, refresh_token, device_0_name, device_0
+var code, access_token, refresh_token, device_0_name, latest_stats
 
 router.get('/', function (req, res) {
+  last_sqlite_entry(function(data) {
+    try {
+      latest_stats = JSON.parse(data)
+    } catch (error) {
+      logger(error, 'red')
+    }
+  })
   res.render('nest_thermostat', {
     title: 'Thermostaat',
     login_url: login_url,
     access_token: access_token,
-    theromostat: device_0
+    stats: latest_stats
   })
 })
 
@@ -44,6 +52,14 @@ router.get('/code', function (req, res) {
         try {
           access_token = JSON.parse(response).access_token
           logger(`Nest API access token refreshed: ${access_token}`,'green')
+          last_sqlite_entry(function(data) {
+            try {
+              latest_stats = JSON.parse(data)
+              //logger(JSON.stringify(latest_stats), 'green')
+            } catch (error) {
+              logger(error, 'red')
+            }
+          })
         } catch (error) {
           logger(error, 'red')
         }
@@ -134,7 +150,7 @@ router.post('/settemperature', function (req, res) {
 function get_token (post_data, callback) {
   const hostname = 'www.googleapis.com'
   const path = '/oauth2/v4/token'
-  post (post_data, hostname, path, undefined, callback)
+  post (post_data, hostname, path, undefined, undefined, true, callback)
 }
 
 function get_devices (callback) {
@@ -152,7 +168,22 @@ function get_device_0 (callback) {
 function set_temperature (post_data, headers, callback) {
   const hostname = 'smartdevicemanagement.googleapis.com'
   const path = '/v1/' + device_0_name + ':executeCommand'
-  post (post_data, hostname, path, headers, callback)
+  post (post_data, hostname, path, headers, undefined, true, callback)
+}
+
+function last_sqlite_entry (callback) {
+  post_data = 'query=SELECT sensehat."index", sense_humidity, sense_temperature,sense_pressure, nest_temperature, nest_humidity, nest_setpoint, temp, humidity, pressure FROM sensehat INNER JOIN weather ON weather."index" = sensehat."index" ORDER BY sensehat."index" DESC LIMIT 1'
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  const hostname = '192.168.68.52'
+  const path = '/api/'
+  const port = 5000
+  try {
+    post (post_data, hostname, path, headers, port, false, callback)
+  } catch (error) {
+    logger(error, 'red')
+  }
 }
 
 function expose_nest_tokens () {
@@ -161,7 +192,13 @@ function expose_nest_tokens () {
   }
 }
 
-function post (post_data, hostname, path, headers=undefined, callback) {
+function post (post_data, hostname, path, headers = undefined, port = undefined, secure = true, callback) {
+  let method
+  if (secure) {
+    method = https
+  } else {
+    method = http
+  }
   let options
   if (typeof headers === 'undefined') {
     options = {
@@ -177,11 +214,17 @@ function post (post_data, hostname, path, headers=undefined, callback) {
       headers: headers
     }
   }
+  if (typeof port !== 'undefined') {
+    options['port'] = port
+  }
   try {
-    const req = https.request(options, (res) => {
+    const req = method.request(options, (res) => {
       let data
       res.setEncoding('utf8')
       res.on('data', (chunk) => {
+        if (typeof chunk === 'object') {
+          chunk = JSON.stringify(chunk)
+        } 
         if (typeof data === 'undefined') {
           data = chunk
         } else {
@@ -240,5 +283,6 @@ function get (hostname, path, callback) {
 module.exports = {
   router: router,
   expose_nest_tokens: expose_nest_tokens,
-  set_temperature: set_temperature
+  set_temperature: set_temperature,
+  last_sqlite_entry: last_sqlite_entry
 }
